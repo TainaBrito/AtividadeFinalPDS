ca0430c720301f9b97710cc343f2d0ef3a7b156f
TBm1512 <108409645+TainaBrito@users.noreply.github.com>
2023-12-15 08:50:40 -0300

    Delete programaTB.py

--- programaTB.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-class MaquiagensDAO:
-
-    def _init_(self):
-        self.conexao = psycopg2.connect(database="TB Makes", user="adm_tbmakes", password="admtbmakes", host="TBMakes.cnbkkiavari1.us-east-1.rds.amazonaws.com", port="5432")
-
-        self.cursor = self.conexao.cursor()
