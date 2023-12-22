78ef6ca00e4e7a974aaa620023166530647e0884
j09mrds <108902833+j09mrds@users.noreply.github.com>
2023-12-22 07:47:04 -0300

    Update maquiagemController.py
    Função inserir

--- maquiagemController.py
+++ maquiagemController.py
@@ -5,3 +5,12 @@ class MaquiagemController:
 
     def __init__(self):
         self.__maquiagem_dao = MaquiagemDAO()
+
+    def inserir(self, cod, n, p, est):
+        maquiagem = MaquiagemBean()
+        maquiagem.setCodigo(cod)
+        maquiagem.setNome(n)
+        maquiagem.setPreco(p)
+        maquinario.setEstado(est)
+
+        self.__maquiagem_dao.inserir_maquiagem(maquiagem)
