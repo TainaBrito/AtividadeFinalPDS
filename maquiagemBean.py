27452d310f7aad1092683a9a14b4202a81d6e191
Taina Brito de Medeiros <medeiros.taina@academico.ifrn.edu.br>
2023-12-22 08:09:04 -0300

    criação da primeira parte do Bean

---
+++ maquiagemBean.py
@@ -0,0 +1,13 @@
+class MaquiagemBean:
+
+    def __init__(self):
+        self.__codigo = None
+        self.__nome = None
+        self.__preco = None
+        self.__estado = None
+
+    def getCodigo(self):
+        return self.__codigo
+
+    def getNome(self):
+        return self.__nome
---
+++ maquiagemController.py
@@ -0,0 +1,33 @@
+from maquiagemBean import MaquiagemBean
+from maquiagemDAO import MaquiagemDAO
+
+class MaquiagemController:
+
+    def __init__(self):
+        self.__maquiagem_dao = MaquiagemDAO()
+
+    def inserir(self, cod, n, p, est):
+        maquiagem = MaquiagemBean()
+        maquiagem.setCodigo(cod)
+        maquiagem.setNome(n)
+        maquiagem.setPreco(p)
+        maquinario.setEstado(est)
+
+        self.__maquiagem_dao.inserir_maquiagem(maquiagem)
+
+    def remover(self, cod):
+        maquiagem = MaquiagemBean()
+        maquiagem.setCodigo(cod)
+        self.__maquiagem_dao.remover_maquiagem(maquiagem)
+
+    def visualizar(self):
+        return self.__maquiagem_dao.visualizar()
+
+    def atualizar(self, cod, n, p, est):
+        maquiagem = MaquiagemBean()
+        maquiagem.setCodigo(cod)
+        maquiagem.setNome(n)
+        maquiagem.setPreco(p)
+        maquiagem.setEstado(est)
+
+        self.__maquiagem_dao.atualizar_maquiagem(maquiagem)
