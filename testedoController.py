a3788814765c04b86c283e44e4466e94e1cdd3bc
Taina Brito de Medeiros <medeiros.taina@academico.ifrn.edu.br>
2023-12-22 08:22:35 -0300

    teste controller

---
+++ testedoController.py
@@ -0,0 +1,16 @@
+from maquiagemController import MaquiagemController
+
+maquiagem_control = MaquiagemController()
+
+#Inserir dados:
+#maquiagem_control.inserir(1525, 'Batom', '10,00', 'Indisponível para venda')
+
+#Atualizar dados:
+#maquiagem_control.atualizar(1525, 'Batom', '12,00', 'Disponível para venda')
+
+#Remover dados:
+#maquiagem_control.remover(1525)
+
+#Visualizar dados:
+#print(maquiagem_control.visualizar())
+
