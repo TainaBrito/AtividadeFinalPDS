dc872e08b52a2f8ea7ad0d2c1265188c9da09f41
Maria Clara Araújo <109550167+ClaraAraujo04@users.noreply.github.com>
2023-12-26 18:35:42 -0300

    Update testedoController.py

--- testedoController.py
+++ testedoController.py
@@ -5,7 +5,12 @@ maquiagem_control = MaquiagemController()
 #Remover dados:
 #maquiagem_control.remover(1525)
 
-#Remover dados:
-#maquiagem_control.remover(1525)
+#Inserir dados:
+#maquiagem_control.inserir(1525, 'Batom', '10,00', 'Indisponível para
+venda')
+
+#Atualizar dados:
+#maquiagem_control.atualizar(1525, 'Batom', '12,00', 'Disponível para
+venda')
 
 
